86f8c6776413849c5ed32da9b525c17e42c34277
Сергей <s.v.yakovlev@yandex.ru>
2022-06-27 23:23:20 +0400

    ДЗ (основные задания)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,60 @@
 public class Main {
+
+    public static String printYear (int year) {
+        int yearDevide4 = year % 4;         //Является високосным
+        int yearDevide100 = year % 100;     //Не является високосным, исключение х400
+        int yearDevide400 = year % 400;     //Является високосным
+
+        if (yearDevide100 == 0 && yearDevide400 != 0) {
+            return year + " - не високосный год";
+        }
+        else if (yearDevide400 == 0 || yearDevide4 == 0) {
+            return year + " - високосный год";
+        }
+        else {
+            return year + " - не високосный год";
+        }
+    }
+
+    public static String printVersion (int clientOS, int clientDeviceYear) {
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                return "Установите облегченую версию приложения для iOS по ссылке";
+            } else {
+                return "Установите версию приложения для iOS по ссылке";
+            }
+        }
+
+        if (clientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                return "Установите облегченую версию приложения для Android по ссылке";
+            } else {
+                return "Установите версию приложения для Android по ссылке";
+            }
+        }
+        else {
+            return "Ошибка ввода данных";
+        }
+    }
+
+    public static void deliveryDay(int distance) {
+        int day = (distance + 19) / 40 + 1;
+        System.out.println(day);
+    }
+
     public static void main(String[] args) {
+// Задача №1
+        int currentYear = 2022;
+        System.out.println(printYear(currentYear));
 
+// адача №2
+        int os = 0;
+        int year = 2017;
+        System.out.println(printVersion(os, year));
 
+// Задача №3
+        int deliveryDistance = 95;
+        System.out.print("Потребуется дней: ");
+        deliveryDay(deliveryDistance);
     }
 }
